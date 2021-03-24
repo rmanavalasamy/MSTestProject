683b5cc2e2dae4ce7597846092dd5bf436b1cfa4
Ravikumar Manavalasamy <ravikumarm@ilink-systems.com>
2021-03-24 16:19:32 +0530

    first commit

---
+++ MSTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91b477a4-c298-4486-a522-d1c462f4c06b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MSTestProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MSTestProject
+{
+    [TestClass]
+    public class Login
+    {
+        //IWebDriver driver = null;
+        IWebDriver driver = new ChromeDriver(@"D:\MSTest\MSTestProject\MSTestProject\Driver");
+
+        [TestInitialize()] //Used to initialize the test before running each Test Method
+        public void Initialize()
+        {
+            // InitializeDriver("CHROME");
+
+            //IWebDriver driver = new ChromeDriver(@"D:\SeleniumRegressionAutomation\SeleniumRegressionAutomation\Driver");
+        }
+
+
+        [TestMethod]
+        [TestCategory("Smoke")]
+        public void Navigate_To_Page()
+        {
+            driver.Navigate().GoToUrl("https://www.guru99.com/use-autoit-selenium.html#1");
+        }
+
+        [TestMethod]
+        [TestCategory("Regression")]
+        public void Perform_Click_Operation_On_Element()
+        {
+            driver.Navigate().GoToUrl("https://gmail.com");           
+        }
+
+    }
+}
